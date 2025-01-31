fe7ca6494b722980df5e9703b42f1246415c3173
Salvador Sanchez-Alonso <salvador.sanchez@uah.es>
2025-01-31 17:50:42 +0100

    Moidificada la solución del ejercicio 3

--- PunterosEj3_RES.pas
+++ PunterosEj3_RES.pas
@@ -1,4 +1,5 @@
 program PunterosEj3_RES;
+
 type
     { Declara un tipo de registro llamado “nodo” con dos campos: un entero y un puntero a “nodo” }
     nodo = record
@@ -12,23 +13,23 @@ var
 
 begin
     { Crea un nodo en memoria dinámica (heap) }
-    New(punteroNodo);
+    new(punteroNodo);
 
     { Dale valor 100 al campo entero de este nodo }
     punteroNodo^.valor := 100;
 
     { Pon a nil el campo puntero del nodo }
-    punteroNodo^.otro_nodo := nil;
+    punteroNodo^.otro_nodo := NIL;
 
     { Pon ahora el puntero del nodo a apuntar al propio nodo }
     punteroNodo^.otro_nodo := punteroNodo;
 
-    { En este punto hay dos instrucciones alternativas para liberar la memoria dinámica creada: }
-    { Alternativa 1: Liberar el nodo directamente }
-    Dispose(punteroNodo);
+    { En este punto hay dos instrucciones alternativas para liberar la memoria dinámica creada pero CUIDADO: 
+      Debes elegir una u otra, pues si incluyes las dos estarás provocando un error al tratar de devolver
+      2 veces la misma memoria!! }
 
-    { Alternativa 2: Liberar el nodo a través del campo puntero }
-    { Dispose(punteroNodo^.otro_nodo); }
+    dispose(punteroNodo); { Alternativa 1: Liberar el nodo desde el puntero que lo reservó inicialmente}
+    dispose(punteroNodo^.otro_nodo); { Alternativa 2: Liberar el nodo a través del campo "otro_nodo" del propio nodo }
 
     { Termina }
 end.
